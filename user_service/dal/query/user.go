391e21bfe3015b0d0e809da4ac303245f9f2de62
Writiger <75297838+writiger@users.noreply.github.com>
2023-02-07 16:02:34 +0800

    添加gitignore缩减体积

--- user_service/dal/query/user.go
+++ user_service/dal/query/user.go
@@ -1,8 +1,20 @@
 package query
 
-import "dousheng_server/user_service/dal/model"
+import (
+	"dousheng_server/snowflake_service/kitex_gen/kitex_gen/snowflake"
+	"dousheng_server/user_service/dal/model"
+	"github.com/cloudwego/kitex/client"
+	etcd "github.com/kitex-contrib/registry-etcd"
+	"log"
+)
 
 func CreateUser(user *model.User) error {
+	r, err := etcd.NewEtcdResolver([]string{"127.0.0.1:2379"}) // r不应重复使用。
+	snowClient, err := snowflake.NewClient("snowflakeservice", client.WithResolver(r))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	if err := GormClient.Create(user).Error; err != nil {
 		return err
 	}
