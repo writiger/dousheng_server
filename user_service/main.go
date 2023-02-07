4b81192fcc9598d3fc1d678aeb9f17cac37b8b20
Writiger <75297838+writiger@users.noreply.github.com>
2023-02-07 11:40:55 +0800

    调整部署端口

--- user_service/main.go
+++ user_service/main.go
@@ -6,16 +6,19 @@ import (
 	"github.com/cloudwego/kitex/server"
 	etcd "github.com/kitex-contrib/registry-etcd"
 	"log"
+	"net"
 )
 
 func main() {
+	addr, _ := net.ResolveTCPAddr("tcp", ":8900")
 	r, err := etcd.NewEtcdRegistry([]string{"127.0.0.1:2379"})
 	if err != nil {
 		log.Fatal(err)
 	}
 	svr := usercenter.NewServer(new(UserCenterImpl),
 		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: "userservice"}),
-		server.WithRegistry(r))
+		server.WithRegistry(r),
+		server.WithServiceAddr(addr))
 
 	err = svr.Run()
 
