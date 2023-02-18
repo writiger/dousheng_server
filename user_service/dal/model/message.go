05bc6b752c9a7aaa622c63405418695e2f2f8343
GXL <2323790876@qq.com>
2023-02-18 22:49:48 +0800

    完成消息部分，成功获取消息列表，但消息列表轮播重复BUG暂未想出解决办法

--- user_service/dal/model/message.go
+++ user_service/dal/model/message.go
@@ -1,16 +1,10 @@
 package model
 
-import (
-	"gorm.io/gorm"
-	"time"
-)
-
 type Message struct {
 	// UserId 关注了 FollowID
-	Id         int64          `gorm:"primaryKey" json:"id"`
-	Messages   string         `gorm:"message" json:"content"`
-	FromUserId int64          `gorm:"from_user_id" json:"from_user_id"`
-	ToUserId   int64          `gorm:"to_user_id" json:"to_user_id"`
-	CreatedAt  time.Time      `json:"create_time"`
-	DeletedAt  gorm.DeletedAt `gorm:"index"`
+	Id         int64  `gorm:"primaryKey" json:"id"`
+	ToUserId   int64  `gorm:"to_user_id" json:"to_user_id"`
+	FromUserId int64  `gorm:"from_user_id" json:"from_user_id"`
+	Messages   string `gorm:"message" json:"content"`
+	CreatedAt  int64  `json:"create_time"`
 }
