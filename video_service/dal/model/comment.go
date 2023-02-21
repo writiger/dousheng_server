414dffe9bdf2fd104315492daf1f9a8f7504ec18
Writiger <75297838+writiger@users.noreply.github.com>
2023-02-21 11:29:52 +0800

    添加索引

--- video_service/dal/model/comment.go
+++ video_service/dal/model/comment.go
@@ -4,9 +4,9 @@ import "time"
 
 type Comment struct {
 	CommentId  int64  `gorm:"primaryKey" json:"comment_id"`
-	UserId     int64  `json:"user_id"`
-	VideoId    int64  `json:"video_id"`
+	UserId     int64  `gorm:"index:idx_user" json:"user_id"`
+	VideoId    int64  `gorm:"index:idx_video" json:"video_id"`
 	Content    string `json:"content"`
-	CreateDate string `json:"create_date"`
+	CreateDate string `gorm:"index:idx_create,sort:desc" json:"create_date"`
 	CreatedAt  time.Time
 }
